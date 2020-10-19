3fa89db8646c2f8a07911b58275cc4a0583d08b2
AmirabbasJ <amirabbasgamer2@gmail.com>
2020-10-19 14:40:09 +0330

    fix: fix validation 🐛

--- server/src/validators/register.js
+++ server/src/validators/register.js
@@ -3,11 +3,12 @@ const Validator = require('fastest-validator');
 const v = new Validator();
 
 const schema = {
-	name: { type: 'string', min: 3, max: 300 },
-	email: { type: 'string', min: 4, max: 300 },
+	username: { type: 'string', min: 3, max: 300, optional: true },
+	name: { type: 'string', min: 3, max: 300, optional: true },
+	email: { type: 'email', min: 4, max: 300 },
 	password: { type: 'string', min: 6, max: 300 },
 	confirmPassword: { type: 'equal', field: 'password', strict: true },
-	$$strict: true,
+	$$strict: 'remove',
 };
 
 const checker = v.compile(schema);
