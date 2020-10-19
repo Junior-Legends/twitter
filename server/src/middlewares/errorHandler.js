138f68c7b7457dca51e6f32c95e33e67877e0b7b
AmirabbasJ <amirabbasgamer2@gmail.com>
2020-10-19 17:32:16 +0330

    refactor: delete status code duplication ♻️

--- server/src/middlewares/errorHandler.js
+++ server/src/middlewares/errorHandler.js
@@ -3,7 +3,6 @@
 function errorHandler(error, _req, res, _next) {
 	return res.status(error.status || 500).send({
 		error: {
-			status: error.status || 500,
 			message: error.message || 'Internal Server Error',
 		},
 	});
