774b1aafa1773703949c528a34459eb9dc5bfb14
AmirabbasJ <amirabbasgamer2@gmail.com>
2020-10-22 18:09:27 +0330

    fix: fix warning 🚨

--- server/src/utils/asyncCatch.ts
+++ server/src/utils/asyncCatch.ts
@@ -1,6 +1,6 @@
 import { Request, Response, NextFunction, RequestHandler } from 'express';
 
 export default (fn: RequestHandler) => {
-	return (req: Request, res: Response, next: NextFunction) =>
+	return (req: Request, res: Response, next: NextFunction): Response =>
 		fn(req, res, next).catch(next);
 };
