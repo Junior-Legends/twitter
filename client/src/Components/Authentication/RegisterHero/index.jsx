4d14f523b3d09a2119ca430b34d1e4ac2a6d8327
AmirabbasJ <amirabbasgamer2@gmail.com>
2020-11-25 00:30:05 +0330

    feat: add specific error messages ✨

--- client/src/Components/Authentication/RegisterHero/index.jsx
+++ client/src/Components/Authentication/RegisterHero/index.jsx
@@ -3,10 +3,14 @@ import "./registerHero.scss";
 import ThemeSwitchButton from "../../ThemeSwitchButton";
 import { Link } from "react-router-dom";
 import { useForm } from "react-hook-form";
+import ValidationErrorMessage from "../../../ValidationErrorMessage";
 
 const RegisterHero = () => {
 	// Handling form with react-hook-form library !
-	const { register, handleSubmit, getValues, errors } = useForm();
+	const { register, handleSubmit, watch, errors } = useForm({
+		mode: "onSubmit",
+		reValidateMode: "onSubmit",
+	});
 	const onSubmit = (data) => console.log(data);
 	// console.log(errors);
 
@@ -20,23 +24,29 @@ const RegisterHero = () => {
 				<form className="register_hero_form" onSubmit={handleSubmit(onSubmit)}>
 					<label htmlFor="">نام</label>
 					<input
+						className="register_hero_form_input"
 						spellCheck={false}
 						type="text"
 						name="name"
 						ref={register({
-							required: true,
+							required: "نام الزامی است",
+
 							pattern: /.{3,300}/i,
 						})}
 					/>
 					<label htmlFor="">ایمیل</label>
 					<input
+						className="register_hero_form_input"
 						spellCheck={false}
 						type="text"
-						name="Email"
+						name="email"
 						ref={register({
-							required: true,
+							required: "ایمیل الزامی است",
 							//^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$
-							pattern: /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/i,
+							pattern: {
+								value: /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/i,
+								message: "ایمیل صحیح نمیباشد",
+							},
 							max: 300,
 							min: 4,
 						})}
@@ -45,14 +55,17 @@ const RegisterHero = () => {
 						<div>
 							<label htmlFor="">رمز عبور</label>
 							<input
+								className="register_hero_form_input"
 								spellCheck={false}
 								type="password"
 								name="password"
 								ref={register({
-									required: true,
-									max: 300,
-									min: 6,
-									pattern: /(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,300}/i,
+									required: "رمز عبور الزامی است",
+									pattern: {
+										value: /(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,300}/i,
+										message:
+											"پسورد باید شامل حروف بزرگ و کوچک ، عدد و حداقل 6 کاراکتر باشد",
+									},
 									// "Must contain at least one number and
 									//one uppercase and lowercase letter, and at least 6 or more characters"
 								})}
@@ -61,34 +74,28 @@ const RegisterHero = () => {
 						<div>
 							<label htmlFor="">تکرار رمزعبور</label>
 							<input
+								className="register_hero_form_input"
 								spellCheck={false}
 								type="password"
 								name="passwordConfirmation"
 								ref={register({
-									validate: {
-										passwordEqual: (value) =>
-											value === getValues().password ||
-											"Password confirmation error!",
+									validate: (value) => {
+										console.log(value, watch("password"));
+										return (
+											value === watch("password") ||
+											"تکرار رمزعبور صحیح نمیباشد"
+										);
 									},
 								})}
 							/>
 						</div>
 					</div>
-
+					<ValidationErrorMessage errors={Object.values(errors)} />
 					<input
 						type="submit"
 						value="ثبت نام"
 						className="register_hero_form_submit"
 					/>
-					{errors.name && <p>نام کاربری را وارد کنید(حداقل 3 حرف)</p>}
-					{errors.Email && <p>ایمیل را درست وارد کنید</p>}
-					{errors.password && <p>پسورد را درست وارد کنید</p>}
-					{errors.password && (
-						<p>پسورد باید شامل حروف بزرگ و کوچک ، عدد و حداقل 6 کاراکتر باشد</p>
-					)}
-					{errors.passwordConfirmation && (
-						<p>تکرار رمزعبور را درست وارد کنید </p>
-					)}
 				</form>
 				<div className="register_hero_login">
 					<Link to="/login" className="register_hero_login_after">
