5c4ecac54c7e73a4fa30cf739295e0219836e1dc
AmirabbasJ <amirabbasgamer2@gmail.com>
2020-11-27 13:48:03 +0330

    feat: add prefer-color-scheme support ✨

--- client/src/Context/Theme/index.jsx
+++ client/src/Context/Theme/index.jsx
@@ -1,15 +1,26 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import themes from "../../Constants/Themes";
+import initPresistTheme from "./initalTheme";
 
 export const ThemeCtx = React.createContext();
 export const ToggleThemeCtx = React.createContext();
 
-const initPresistTheme = localStorage.getItem("theme");
-
 export const ThemeProvider = ({ children }) => {
 	const [theme, setTheme] = useState(initPresistTheme || themes.dark);
+
+	useEffect(() => {
+		const preferThemeChangeHandler = (e) => {
+			const newColorScheme = e.matches ? themes.dark : themes.light;
+			setTheme(newColorScheme);
+			localStorage.setItem("theme", newColorScheme);
+		};
+
+		const preferDarkMedia = window.matchMedia("(prefers-color-scheme: dark)");
+		preferDarkMedia.addEventListener("change", preferThemeChangeHandler);
+	}, []);
+
 	const toggleTheme = () => {
-		setTheme(prevTheme => {
+		setTheme((prevTheme) => {
 			let newTheme = themes.dark;
 
 			if (prevTheme === themes.dark) {
@@ -19,6 +30,7 @@ export const ThemeProvider = ({ children }) => {
 			return newTheme;
 		});
 	};
+
 	return (
 		<ThemeCtx.Provider value={theme}>
 			<ToggleThemeCtx.Provider value={toggleTheme}>
