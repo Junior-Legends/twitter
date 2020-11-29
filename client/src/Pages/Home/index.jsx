1c737e60cd7e8496bd55e70ad5242a3c38c874ae
AmirabbasJ <amirabbasgamer2@gmail.com>
2020-11-29 18:53:26 +0330

    refactor: extract pages component back to their main container ♻️

--- client/src/Pages/Home/index.jsx
+++ client/src/Pages/Home/index.jsx
@@ -2,9 +2,14 @@ import React from "react";
 
 // import Main Components
 import SideNavbar from "../../Components/SideNavbar";
-import TopTwits from "../../Components/TopTwits";
+import {
+	usersInfo as dummyUsersInfo,
+	amin,
+} from "../../Constants/Dummies/userData.dummy";
 import SideBar from "../../Components/SideBar";
 import Header from "../../Components/Header";
+import Twits from "../../Components/Twits";
+import ResizableTextarea from "../../Components/ResizableTextarea";
 
 const Home = () => {
 	return (
@@ -12,7 +17,27 @@ const Home = () => {
 			<SideNavbar />
 			<div className="home_top_twits_wrapper">
 				<Header title="خانه" />
-				<TopTwits />
+				<section className="top_twits_user_wrapper">
+					<div className="top_twits_user">
+						<div className="top_twits_user_top_elements">
+							<img
+								className="top_twits_user_profile_image"
+								src={amin.pic}
+								alt="aks"
+							/>
+							<ResizableTextarea
+								style={{ marginRight: ".5rem" }}
+								placeholder="چه اتفاقی افتاده است؟"
+							></ResizableTextarea>
+						</div>
+						<div className="top_twits_user_bottom_elements">
+							<i className="icon-Image"></i>
+							<button>توییت</button>
+						</div>
+					</div>
+				</section>
+
+				<Twits usersInfo={dummyUsersInfo} />
 			</div>
 			<SideBar />
 		</div>
--- client/src/Pages/Register/index.jsx
+++ client/src/Pages/Register/index.jsx
@@ -1,15 +1,112 @@
 import React from "react";
+import { useForm } from "react-hook-form";
+import { Link } from "react-router-dom";
 
-// import Main Components
-import Sidebar from "../../Components/Authentication/Sidebar";
-import RegisterHero from "../../Components/Authentication/RegisterHero";
+import Promo from "../../Components/Promo";
+import ThemeSwitchButton from "../../Components/ThemeSwitchButton";
+import ValidationErrorMessage from "../../ValidationErrorMessage";
 
 const Login = () => {
+	const { register, handleSubmit, watch, errors } = useForm({
+		mode: "onSubmit",
+		reValidateMode: "onSubmit",
+	});
+	const onSubmit = (data) => console.log(data);
 	return (
 		<>
 			<div className="register">
-				<Sidebar />
-				<RegisterHero />
+				<Promo />
+				<div className="register_hero">
+					<div className="register_hero_theme_switch_wrapper">
+						<ThemeSwitchButton />
+					</div>
+					<div className="register_hero_entry_wrapper">
+						<h4>امروز به توییتر فارسی بپیوندید.</h4>
+						<form
+							className="register_hero_form"
+							onSubmit={handleSubmit(onSubmit)}
+						>
+							<label htmlFor="">نام</label>
+							<input
+								className="register_hero_form_input"
+								spellCheck={false}
+								type="text"
+								name="name"
+								ref={register({
+									required: "نام الزامی است",
+
+									pattern: /.{3,300}/i,
+								})}
+							/>
+							<label htmlFor="">ایمیل</label>
+							<input
+								className="register_hero_form_input"
+								spellCheck={false}
+								type="text"
+								name="email"
+								ref={register({
+									required: "ایمیل الزامی است",
+									pattern: {
+										value: /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/i,
+										message: "ایمیل صحیح نمیباشد",
+									},
+									max: 300,
+									min: 4,
+								})}
+							/>
+							<div className="register_hero_form_password_container">
+								<div>
+									<label htmlFor="">رمز عبور</label>
+									<input
+										className="register_hero_form_input"
+										spellCheck={false}
+										type="password"
+										name="password"
+										ref={register({
+											required: "رمز عبور الزامی است",
+											pattern: {
+												value: /(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,300}/i,
+												message:
+													"پسورد باید شامل حروف بزرگ و کوچک ، عدد و حداقل 6 کاراکتر باشد",
+											},
+											// "Must contain at least one number and
+											//one uppercase and lowercase letter, and at least 6 or more characters"
+										})}
+									/>
+								</div>
+								<div>
+									<label htmlFor="">تکرار رمزعبور</label>
+									<input
+										className="register_hero_form_input"
+										spellCheck={false}
+										type="password"
+										name="passwordConfirmation"
+										ref={register({
+											validate: (value) => {
+												console.log(value, watch("password"));
+												return (
+													value === watch("password") ||
+													"تکرار رمزعبور صحیح نمیباشد"
+												);
+											},
+										})}
+									/>
+								</div>
+							</div>
+							<ValidationErrorMessage errors={Object.values(errors)} />
+							<input
+								type="submit"
+								value="ثبت نام"
+								className="register_hero_form_submit"
+							/>
+						</form>
+						<div className="register_hero_login">
+							<Link to="/login" className="register_hero_login_after">
+								قبلا عضو شده اید؟ ورود
+							</Link>
+						</div>
+					</div>
+				</div>
 			</div>
 		</>
 	);
