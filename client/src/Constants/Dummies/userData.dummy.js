a44cafe8109900062053089180d9bc44dea6cefc
AmirabbasJ <amirabbasgamer2@gmail.com>
2020-11-29 19:07:29 +0330

    refactor: seperate dummy data ♻️

---
+++ client/src/Constants/Dummies/userData.dummy.js
@@ -0,0 +1,27 @@
+import aminPic from "../../Assets/images/amin.png";
+import hamedPic from "../../Assets/images/hamed.png";
+import amirPic from "../../Assets/images/amirly.png";
+import twitt_pic from "../../Assets/images/twitt_pic.png";
+
+export const amir = {
+	name: "Amir",
+	pic: amirPic,
+	userId: "@AmirhBeigi",
+	likes: 200,
+	verified: true,
+};
+export const amin = {
+	name: "Amin",
+	pic: aminPic,
+	userId: "amin@",
+	like: 150,
+	verified: true,
+};
+export const hamed = {
+	name: "Hamed",
+	pic: hamedPic,
+	userId: "hamed@",
+	like: 350,
+	image: twitt_pic,
+};
+export const usersInfo = [amin, hamed, hamed];
