9ea375456e752c699f1a2a8bfda60ee48359220d
AmirabbasJ <amirabbasgamer2@gmail.com>
2020-12-03 01:45:20 +0330

    refactor: rename MinContent to Main ♻️

--- client/src/Components/MainContent/index.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import "./MainContent.scss";
-
-const MainContent = ({ children }) => {
-	return <div className="main_content">{children}</div>;
-};
-
-export default MainContent;
