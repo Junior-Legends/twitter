00a36b22847f207da878eca66afc31601ee5c8e8
AmirabbasJ <amirabbasgamer2@gmail.com>
2020-12-02 18:59:18 +0330

    refactor: rename sidebar to leftSideContent ♻️

---
+++ client/src/Components/LeftSideContent/index.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./LeftSideContent.scss";
+
+const LeftSideContent = ({ children }) => {
+	return <div className="left-side-content_container">{children}</div>;
+};
+
+export default LeftSideContent;
