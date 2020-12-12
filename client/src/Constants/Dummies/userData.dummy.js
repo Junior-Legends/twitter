34298db9ae58ad971f5e4157a312235fc93ce2fe
AmirabbasJ <amirabbasgamer2@gmail.com>
2020-12-12 23:25:55 +0330

    feat: add profile picture image to dummy data 🍱

--- client/src/Constants/Dummies/userData.dummy.js
+++ client/src/Constants/Dummies/userData.dummy.js
@@ -3,13 +3,14 @@ import hamedPic from "../../Assets/images/hamed.png";
 import amirPic from "../../Assets/images/amirly.png";
 import twitt_pic from "../../Assets/images/twitt_pic.png";
 import post_image from "../../Assets/images/post_image.jpg";
+import big_prof from "../../Assets/images/big-prof.png";
 
 const amir = {
 	name: "Amir",
 	pic: amirPic,
 	username: "AmirhBeigi@",
 	likes: 200,
-	verified: true
+	verified: true,
 	// logged: true
 };
 const amin = {
@@ -17,15 +18,15 @@ const amin = {
 	pic: aminPic,
 	username: "amin@",
 	like: 150,
-	verified: true
+	verified: true,
 };
 const hamed = {
 	name: "Hamed",
 	pic: hamedPic,
 	username: "hamed@",
 	like: 350,
-	image: twitt_pic
+	image: twitt_pic,
 };
 const usersInfo = [amin, hamed, amir];
 
-export { usersInfo, amin, amir, hamed, post_image };
+export { usersInfo, amin, amir, hamed, post_image, big_prof };
